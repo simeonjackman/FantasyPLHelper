d1aa9662fea4f4e91a06b2c9a4c14a7ac6f9e8f6
Simeon Tim Jackman <simja114@student.liu.se>
2020-10-05 16:39:26 +0200

    ADD: setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='FantasyPLHelper',
+    version='0.0dev',
+    packages=['fantasyplhelper',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
