2e62318bd4d39f15a541d97bb50e7c98d5d90bfc
Simeon Tim Jackman <simja114@student.liu.se>
2020-10-05 16:52:39 +0200

    FIX: txt to md

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0dev',
     packages=['fantasyplhelper',],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
